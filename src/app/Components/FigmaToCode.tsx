9ce28bf76e9ac276638273addf9504505c0f8c58
Ghufran021 <muhammadghufran206@gmail.com>
2024-12-10 00:17:58 +0500

    initial commit

---
+++ src/app/Components/FigmaToCode.tsx
@@ -0,0 +1,237 @@
+import Image from "next/image"
+import { Eye, Heart } from 'lucide-react';
+
+const FigmaToCode = () => {
+    return (
+        <div>
+            <div className="flex justify-start items-center p-5 gap-2">
+            <div className="bg-[#F5F5F5] border border-slate-600 w-[270px] h-[330px] rounded-lg mb-20 mt-10">
+                <div className="flex justify-end p-5 w-full">
+                    <Heart />
+                </div>
+                
+                <div className="flex justify-end pr-5  w-fill">
+                    <Eye />
+                    </div>
+                <div className="flex justify-center">
+                    <Image src="/Frame 604 (1).png" alt="ProductOne" width={190} height={180} pb-10/>
+                </div>
+
+                <h3 className="text-center pt-4 font-medium text-base mt-1">
+                    Breed Dry Dog Food
+                </h3>
+
+                <div className="flex justify-center items-center space-x-4">
+  <div className="text-center pt-4 font-medium text-base , flex items-center space-x-1">
+    <div>$100</div>
+    
+      
+      <div>
+        </div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-gray-300">&#9733;</div>
+      <div className="text-gray-300">&#9733;</div>
+      <div className="text-font-medium text-base">(35)</div>
+  </div>
+  </div>
+  
+</div>
+            
+            <div className="bg-[#F5F5F5] border border-blue-600 w-[300px] h-[370px] rounded-lg">
+                <div className="flex justify-end p-5 w-fill">
+                    <Heart />
+                </div>
+                <div className="flex justify-end pr-5  w-fill">
+                    <Eye />
+                </div>
+                <div className="flex justify-around">
+                    <Image src="/Camera.png" alt="ProductOne" width={190} height={180} />
+                </div>
+
+                <div className="flex justify-center">
+                    <button className="bg-[#000000] rounded-lg pt-3 mt-5 pb-3 bg-black text-white w-[300px] h-[41]">
+                        Add To Cart
+                    </button>
+                
+                </div>
+                <h3 className="text-center pt-4 font-medium text-base width={208} height={24}">
+                    CANON EOS DSLR Camera
+                </h3>
+
+                <div className="flex justify-center items-center space-x-4">
+  <div className="text-center pt-4 font-medium text-base , flex items-center space-x-1">
+    <div>$360</div>
+    
+      
+      <div>
+        </div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-gray-300">&#9733;</div>
+      <div className="text-font-medium text-base">(95)</div>
+  </div>
+  </div>
+  
+</div>
+
+<div className="bg-[#F5F5F5] border border-purple-600 w-[270px] h-[290px] rounded-lg mb-20">
+                <div className="flex justify-end p-5 w-full">
+                    <Heart  />
+                </div>
+                <div className="flex justify-end pr-5  w-fill">
+                    <Eye />
+                </div>
+
+                <div className="flex justify-center mb-10 ">
+                    <Image src="/Frame 604.png" alt="ProductOne" width={190} height={180} />
+                </div>
+
+                <h3 className="text-center pt-4 font-medium text-base">
+                ASUS FHD Gaming Laptop
+                </h3>
+<div className="flex justify-center items-center space-x-4">
+    <div className="text-center pt-4 font-medium text-base , flex items-center space-x-1">
+    <div>$700</div>
+    
+      
+      <div>
+        </div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-font-medium text-base">(325)</div>    
+    </div>
+</div>
+
+
+            </div>
+            
+            <div className="bg-[#F5F5F5] border border-blue-600 w-[270px] h-[322px] rounded-lg mb-12">
+                <div className="flex justify-end p-5 w-fill">
+                    <Heart />
+                </div>
+                <div className="flex justify-end pr-5  w-fill">
+                    <Eye />
+                </div>
+                <div className="flex justify-around">
+                    <Image src="/curology.png" alt="ProductOne" width={172} height={159} />
+                </div>
+
+                
+                <h3 className="text-center pt-4 font-medium text-base width={208} height={24}">
+                    Curology Product Set
+                </h3>
+
+                <div className="flex justify-center items-center space-x-4">
+  <div className="text-center pt-4 font-medium text-base , flex items-center space-x-1">
+    <div>$500</div>
+    
+      
+      <div>
+        </div>
+        <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-gray-300">&#9733;</div>
+      <div className="text-font-medium text-base">(145)</div>    
+    
+  </div>
+  </div>
+  
+</div>
+
+            </div>
+
+            <div className="flex justify-start items-center p-5 mt-5 gap-2">
+            <div className="bg-[#F5F5F5] border border-blue-600 w-[270px] h-[270px] rounded-lg mb-12">
+                <div className="flex justify-end p-5 w-fill">
+                    <Heart />
+                </div>
+                <div className="flex justify-end pr-5  w-fill">
+                    <Eye />
+                </div>
+
+                <div className="flex justify-around">
+                    <Image src="/NEW.png" alt="ProductOne" width={27} height={18} />
+                </div>
+
+
+                <div className="flex justify-around">
+                    <Image src="/Electric Toy Car.png" alt="ProductOne" width={150} height={113} />
+                </div>
+
+                
+                
+                
+                <h3 className="text-center pt-8 font-medium text-base width={208} height={24}">
+                    Electric Toy Car
+                </h3>
+
+                <div className="flex justify-center items-center space-x-4">
+  <div className="text-center pt-4 font-medium text-base , flex items-center space-x-1">
+    <div>$960</div>
+    
+      
+      <div>
+        </div>
+        <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-font-medium text-base">(65)</div>
+
+      </div>
+      </div>
+      </div>
+
+
+      <div className="bg-[#F5F5F5] border border-blue-600 w-[250px] h-[270px] rounded-lg mb-12">
+                <div className="flex justify-end p-5 w-fill">
+                    <Heart />
+                </div>
+                <div className="flex justify-end pr-5  w-fill">
+                    <Eye />
+                </div>
+                <div className="flex justify-around">
+                    <Image src="/Copa Sense.png" alt="ProductOne" width={184} height={164} />
+                </div>
+
+                
+                
+                <h3 className="text-center pt-4 font-medium text-base width={208} height={24}">
+                    Jr. Zoom Soccer Cleats
+                </h3>
+
+                <div className="flex justify-center items-center space-x-4">
+  <div className="text-center pt-4 font-medium text-base , flex items-center space-x-1">
+    <div>$1160</div>
+    
+      
+      <div>
+        </div>
+        <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-yellow-500">&#9733;</div>
+      <div className="text-font-medium text-base">(35)</div>    
+
+        </div>
+        </div>
+        </div>
+        </div>
+        
+        </div>
+        
+    )
+}
+
+export default FigmaToCode
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import FigmaToCode from "./Components/FigmaToCode";
+import Navbar from "./Components/Navbar";
+
+export default function Home() {
+  return (
+    <main className="bg-red-700">
+      
+      <FigmaToCode />
+
+    </main>
+  );
+}
